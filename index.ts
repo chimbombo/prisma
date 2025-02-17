5c4b935d58ef03d25fcc1471737dc5bf8fa5d3a9
Antonio Galaz <antonio.galaz.aroca@gmail.com>
2025-02-17 18:48:16 -0300

    FIND

--- index.ts
+++ index.ts
@@ -4,13 +4,25 @@ import { log } from "console";
 const db = new PrismaClient();
 
 const main = async () => {
-    const response = await db.user_prisma.create({
-        data: {
-            email: "jojo@jojo.com",
-            name: "Jojo",
-            lastname: "Jojoland",
-        },
-    });
+
+    // CREATE
+    // const response = await db.user_prisma.create({
+    //     data: {
+    //         email: "jojo@jojo.com",
+    //         name: "Jojo",
+    //         lastname: "Jojoland",
+    //     },
+    // });
+
+    // FIND ALL
+    // const response = await db.user_prisma.findMany();
+
+    // FIND ONE
+    const response = await db.user_prisma.findFirst({
+        where: {
+            id: 1
+        }
+    })
     console.log({ response });
 };
-main()
+main();
